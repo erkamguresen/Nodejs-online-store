9570a682a71bac53cc07f3d35bd2ee1077043780
ERKAM GURESEN <erkamguresen@gmail.com>
2021-08-18 01:28:09 -0700

    5 th chapter

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/user");
+
+const path = require("path");
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+
+app.use(userRoutes);
+
+//can delete later
+app.set("title", "My Site");
+console.log(app.get("title"));
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./views/404.html"));
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000:\nhttp://localhost:3000/");
+});
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const path = require("path");
+
+router.get("/addproduct", (req, res, next) => {
+  //   res.sendFile(path.join(__dirname, "../views/add-product.html"));
+  res.render("add-product");
+});
+
+router.post("/addproduct", (req, res, next) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+module.exports = router;
