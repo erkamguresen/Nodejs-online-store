6517b54f89bc3550f56d999ce5b80bee37f56c41
ERKAM GURESEN <erkamguresen@gmail.com>
2021-08-23 02:53:02 -0700

    product list

--- models/product.js
+++ models/product.js
@@ -1,4 +1,29 @@
-const products = [];
+const products = [
+  {
+    name: "IPhone 7",
+    price: 700,
+    imageURL: "1.jpg",
+    description: "goed phone",
+  },
+  {
+    name: "IPhone 8S",
+    price: 800,
+    imageURL: "2.jpg",
+    description: "very goed phone",
+  },
+  {
+    name: "Samsung S8",
+    price: 300,
+    imageURL: "3.jpg",
+    description: "it is a ok phone",
+  },
+  {
+    name: "Samsung S7",
+    price: 250,
+    imageURL: "4.jpg",
+    description: "just a phone nothing more complicated",
+  },
+];
 
 module.exports = class Product {
   constructor(name, price, imageURL, description) {
