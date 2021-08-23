6ebfc1faddf426a8b7a986a3aefd58d842ad7158
ERKAM GURESEN <erkamguresen@gmail.com>
2021-08-23 03:12:15 -0700

    product id temprory

--- models/product.js
+++ models/product.js
@@ -1,23 +1,27 @@
 const products = [
   {
+    id: "93984",
     name: "IPhone 7",
     price: 700,
     imageURL: "1.jpg",
     description: "goed phone",
   },
   {
+    id: "66869",
     name: "IPhone 8S",
     price: 800,
     imageURL: "2.jpg",
     description: "very goed phone",
   },
   {
+    id: "66885",
     name: "Samsung S8",
     price: 300,
     imageURL: "3.jpg",
     description: "it is a ok phone",
   },
   {
+    id: "21193",
     name: "Samsung S7",
     price: 250,
     imageURL: "4.jpg",
@@ -27,6 +31,7 @@ const products = [
 
 module.exports = class Product {
   constructor(name, price, imageURL, description) {
+    this.id = (Math.floor(Math.random() * 99999) + 1).toString();
     this.name = name;
     this.price = price;
     this.imageURL = imageURL;
